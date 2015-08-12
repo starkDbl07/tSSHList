908514164e1a1feb63880a169e524cb2812ad455
starkDbl07 <abhi.emailto@gmail.com>
2015-08-12 17:49:40 +0545

    ui final-1

--- tSSHList.py
+++ tSSHList.py
@@ -42,59 +42,88 @@ class hostData:
 class ui:
 	searchSectionHeight = 3
 	searchIconWidth = 3
+	totalEntries = 0
+	hostListLimit = 0
+	curPointer = -1
+	searchIcon = u"\U0001F50D".encode('utf-8').strip()
 
-	def __init__(self, mainScreen, cWidth = 70, cHeight = 20):
-		self.mainScreen = mainScreen
-		self.sHeight, self.sWidth = stdscr.getmaxyx()
+	def __init__(self, winScreen, hostList=[], cWidth = 70, cHeight = 20):
+		# Set content and screen height and widths
 		self.cWidth = cWidth
 		self.cHeight = cHeight
+		self.sHeight, self.sWidth = winScreen.getmaxyx()
 
-		self.posCenterH = (self.sHeight / 2) - (self.cHeight / 2)
-		self.posCenterW = (self.sWidth / 2) - (self.cWidth / 2)
+		# Initialize other object properties
+		self.winScreen = winScreen
 
-		mainWin = cs.newwin(cHeight,cWidth+2,self.posCenterH,self.posCenterW-1)
-		mainWin.bkgd(" ", cs.color_pair(1))
+		# Call initialization functions
+		self.placeWindows()
+		self.updateHostList(hostList)
+		self.nextHost()
+		self.winList.refresh()
+		self.winList.getch()
+		self.winSearchText.move(0,0)
+		cs.curs_set(1)
 
-		contentWin = mainWin.derwin(cHeight, cWidth, 0, 1)
-		contentWin.bkgd(" ", cs.color_pair(1))
+	def placeWindows(self):
+		# Get object content/screen height and widths
+		cWidth = self.cWidth
+		cHeight = self.cHeight
+		sWidth = self.sWidth
+		sHeight = self.sHeight
 
-		searchSection = contentWin.derwin(self.searchSectionHeight,cWidth,0,0)
-		searchSection.bkgd(".", cs.color_pair(1))
+		# Set center positions
+		posCenterH = (sHeight / 2) - (cHeight / 2)
+		posCenterW = (sWidth / 2) - (cWidth / 2)
 
+		# Create winWrapper window
+		winWrapper = cs.newwin(cHeight,cWidth+2,posCenterH,posCenterW-1)
+		winWrapper.bkgd(" ", cs.color_pair(1))
+
+		# Create winContent window
+		winContent = winWrapper.derwin(cHeight, cWidth, 0, 1)
+		winContent.bkgd(" ", cs.color_pair(1))
+
+		# Create SearchSection Wrapper Window
+		searchSection = winContent.derwin(self.searchSectionHeight,cWidth,0,0)
+		searchSection.bkgd(" ", cs.color_pair(1))
+
+		# Create SearchIcon Window
 		searchIconSection = searchSection.derwin(self.searchSectionHeight, self.searchIconWidth, 0,0)
+		searchIconSection.addstr(1,1,self.searchIcon)
+
+		# Create SearchText Wrapper Window
 		searchTextSection = searchSection.derwin(self.searchSectionHeight, cWidth - self.searchIconWidth, 0, self.searchIconWidth)
+
+		# Create SearchText Window
 		winSearchText = searchTextSection.derwin(1, cWidth - self.searchIconWidth, 1, 0)
-		winSearchText.bkgd(" ");
+		#winSearchText.bkgd(" ");
 
-		winListSection = contentWin.derwin(cHeight - self.searchSectionHeight, cWidth, self.searchSectionHeight, 0)
-		winListSection.bkgd("-", cs.color_pair(1))
+		# Create HostList Window and its Properties
+		self.hostListLimit = cHeight - self.searchSectionHeight
+		winList = winContent.derwin(self.hostListLimit, cWidth, self.searchSectionHeight, 0)
+		winList.bkgd(" ", cs.color_pair(1))
 
-		mainWin.refresh()
-		contentWin.refresh()
+		# Refresh all windows
+		winWrapper.refresh()
+		winContent.refresh()
 		searchSection.refresh()
 		searchIconSection.refresh()
 		searchTextSection.refresh()
 		winSearchText.refresh()
-		winListSection.refresh()
-		mainWin.getch()
+		winList.refresh()
 
-		self.mainWin = mainWin
-		self.searchSection = searchSection
-		sys.exit(0)
-
-		self.winSearchSection = cs.newwin(1,cWidth,2,0)
-		self.winSearchSection.addstr(self.searchLine)
-		self.winSearchSection.refresh()
-		self.winSearch = cs.newwin(1,cWidth-2,2,2)
-
-		self.winList = cs.newwin(cHeight,cWidth,4,0)
-
-		self.winTitle.addstr("SSH Hosts")
-
-		self.winSearch.refresh()
-		self.winTitle.refresh()
+		# Set windows as object properties
+		self.winWrapper = winWrapper
+		self.winContent = winContent
+		self.winSearchText = winSearchText
+		self.winList = winList
 
 
+	def nextHost(self):
+		self.curPointer = self.curPointer + 1
+		self.winWrapper.chgat(self.curPointer + self.searchSectionHeight,0,-1,cs.color_pair(2))
+		self.winWrapper.refresh()
 
 	def highlighHost(self, pos):
 		if self.curPointer > -1:
@@ -107,11 +136,11 @@ class ui:
 			self.curPointer = pos
 			self.winList.chgat(pos, 0, -1, cs.A_STANDOUT)
 
-	def updateHostList(self):
-		self.totalEntries = len(self.searchHosts) - 1
-		print self.totalEntries
+	def updateHostList(self, hosts):
+		limitedHosts = hosts[0:self.hostListLimit-1]
+		self.totalEntries = len(limitedHosts) - 1
 		self.winList.clear()
-		for host in self.searchHosts:
+		for host in limitedHosts:
 			self.winList.addstr(host + "\n")
 		self.winList.refresh()
 
@@ -206,8 +235,9 @@ cs.curs_set(0)
 #self.stdscr = stdscr
 cs.start_color()
 cs.init_pair(1, cs.COLOR_BLACK, cs.COLOR_WHITE)
+cs.init_pair(2, cs.COLOR_WHITE, cs.COLOR_BLUE)
 
-ui = ui(stdscr)
+ui = ui(stdscr, hosts.getAllHostList())
 
 #ui.searchHosts = hosts.getAllHostList()[0:]
 #ui.updateHostList()
@@ -215,7 +245,7 @@ ui = ui(stdscr)
 #ui.navigate()
 
 cs.endwin()
-
+sys.exit(0)
 if exitHost != "":
 	if ui.accessType == 0:
 		os.execvp("ssh", ["ssh", ui.exitHost])
